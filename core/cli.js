e174283065c0f71cea1159823c71ef83e6c3a152
aikane <sa_yang@bk.ru>
2026-02-09 18:54:25 +0300

    ok

--- core/cli.js
+++ core/cli.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
-
-//import { runTask } from './task.js';
+import { runTask } from './task.js';
 import { consoleDiff } from './diff.js';
 import { rollback } from './rollback.js';
+import { install } from './install.js';
 import { cfg } from './cfgloader.js';
 
-console.log(process.argv)
-console.log(process.cwd())
+const command = process.argv[2];
+
+switch(command) {
+  case 'diff':
+    consoleDiff();
+    break;
+  case 'rollback':
+    rollback();
+    break;
+  case 'install':
+    install();
+    break;
+  case undefined:
+  case 'task':
+    runTask();
+    break;
+  default:
+    console.log('Неизвестная команда:', command);
+    console.log('Доступно: ag [diff|rollback|install|task]');
+}
